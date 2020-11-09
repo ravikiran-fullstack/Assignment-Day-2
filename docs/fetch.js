56cf89fb33c8b6f1da930f95537ac89d4aa78698
kiran-code <ravikiransjce.code@gmail.com>
2020-11-09 10:07:45 +0530

    Added Loading spinner while loading data

--- docs/fetch.js
+++ docs/fetch.js
@@ -1,9 +1,13 @@
 function retrieveCountriesDataFetch(){
   document.getElementById('xmlRequestButton').disabled = true;
+  document.getElementById('loadingSpinner').classList = 'lds-default u-center-block';
   const url = 'https://restcountries.eu/rest/v2/all';
 
   fetch(url)
     .then(res => res.json())
-    .then(data => {createCountriesDataTable(data)})
+    .then(data => {
+      document.getElementById('loadingSpinner').classList.remove('lds-default');
+      createCountriesDataTable(data)
+    })
     .catch(err => console.log(err));
 }
--- docs/xmlhttprequest.js
+++ docs/xmlhttprequest.js
@@ -1,5 +1,6 @@
 function retrieveCountriesDataXml() {
   document.getElementById('fetchRequestButton').disabled = true;
+  document.getElementById('loadingSpinner').classList = 'lds-default u-center-block';
   let req = new XMLHttpRequest(); // 1. Create a new XMLHttpRequest object
   let countriesData;
   req.open("GET", "https://restcountries.eu/rest/v2/all"); // 2. Initialize it: GET-request for the URL
@@ -14,6 +15,7 @@ function retrieveCountriesDataXml() {
       //console.log('response', JSON.parse(req.response));
       countriesData = JSON.parse(req.response);
       createCountriesDataTable(countriesData);
+      document.getElementById('loadingSpinner').classList.remove('lds-default');
       //console.log(`Done, got ${req.response.length} bytes`);
     }
   };
